8cab72810a9805552d9422e80fcee230e04561fa
ChasNicholls <46545822+ChasNicholls@users.noreply.github.com>
2025-07-25 11:58:31 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const CHORDS = ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim'];
 const chordDiagramPath = 'chord-diagrams';
+
 function generateChords() {
   const key = document.getElementById("keySelect").value;
-  const chords = getChordsForKey(key);
-  const textArea = document.getElementById("lyricsInput");
-  textArea.value += "\n\n" + chords.join(" ");
-}
-function getChordsForKey(key) {
-  const KEYS = {
+  const chords = {
     C: ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'Bdim'],
     G: ['G', 'Am', 'Bm', 'C', 'D', 'Em', 'F#dim'],
     D: ['D', 'Em', 'F#m', 'G', 'A', 'Bm', 'C#dim'],
@@ -15,20 +10,24 @@ function getChordsForKey(key) {
     E: ['E', 'F#m', 'G#m', 'A', 'B', 'C#m', 'D#dim'],
     F: ['F', 'Gm', 'Am', 'Bb', 'C', 'Dm', 'Edim'],
     Bb: ['Bb', 'Cm', 'Dm', 'Eb', 'F', 'Gm', 'Adim']
-  };
-  return KEYS[key] || CHORDS;
+  }[key] || [];
+  document.getElementById("lyricsInput").value += "\n\n" + chords.join(" ");
 }
+
 function renderSong() {
   const input = document.getElementById("lyricsInput").value;
   const output = document.getElementById("renderedSong");
   const diagramContainer = document.getElementById("chordDiagrams");
   diagramContainer.innerHTML = '';
   const usedChords = new Set();
+
   const rendered = input.replace(/\[([A-G][#b]?m?(maj7|7|sus4|sus2)?)\]/g, (match, chord) => {
     usedChords.add(chord);
     return `<span class="chord">[${chord}]</span>`;
   });
+
   output.innerHTML = rendered;
+
   usedChords.forEach(chord => {
     const img = document.createElement('img');
     img.src = `${chordDiagramPath}/${chord}.png`;
@@ -37,6 +36,7 @@ function renderSong() {
     diagramContainer.appendChild(img);
   });
 }
+
 async function downloadPDF() {
   const { jsPDF } = window.jspdf;
   const doc = new jsPDF();
